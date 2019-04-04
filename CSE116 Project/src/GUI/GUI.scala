ae6bb50b190d0237d650f208013ef112dc314f21
Dig Bik <jchan43@buffalo.edu>
2019-04-04 16:02:35 -0400

    test

--- CSE116 Project/src/GUI/GUI.scala
+++ CSE116 Project/src/GUI/GUI.scala
@@ -12,21 +12,30 @@ import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.geometry.Insets
 import scalafx.scene.Scene
+import scalafx.scene.image.Image
 import scalafx.scene.effect.DropShadow
 import scalafx.scene.layout.HBox
 import scalafx.scene.paint.Color._
-import scalafx.scene.paint.{Stops, LinearGradient}
+import scalafx.scene.layout.GridPane
+import scalafx.scene.control.Label
+import scalafx.scene.input.KeyEvent
+import scalafx.scene.paint.{LinearGradient, Stops}
 import scalafx.scene.text.Text
 
 
-
 object GUI extends JFXApp{
-  stage = new application.JFXApp.PrimaryStage{
-    title = "Retro Quiz"
-    var picture = new Canvas(200, 200)
-    var screen = picture.getGraphicsContext2D
-    screen.fillRect(0,0,300,300)
-    scene = new Scene(200, 200){
+  stage = new JFXApp.PrimaryStage {
+    //    g.fillRect(0, 0, 300, 300) //black rectangle
+    scene = new Scene(600, 600) {
+      title = "Retro Quiz"
+      var canvas = new Canvas(600, 600)
+      var g = canvas.getGraphicsContext2D
+      //    var image = new Image("file:images")
+      var player = new Player(new Image("file:images/thanos.png"), new util(275, 550))
+      var right = false
+      var left = false
+      var up = false
+      var down = false
       fill = LightGreen
       content = new Rectangle {
         x = 12.5
@@ -34,18 +43,40 @@ object GUI extends JFXApp{
         width = 50
         height = 50
       }
-      val playerlogin = new TextInputDialog(defaultValue = ""){
-        initOwner(stage)
-        title = "Enter Your Username"
-        headerText = "Enter Your Username"
-        contentText = "please enter your name here"
+//      val playerlogin = new TextInputDialog(defaultValue = "") {
+//        initOwner(stage)
+//        title = "Enter Your Username"
+//        headerText = "Enter Your Username"
+//        contentText = "Please Enter Your Name Here"
+//      }
+//      val result = playerlogin.showAndWait()
+//      result match {
+//        case Some(name) => println("Your name: " + name)
+//        case None => println("Dialog was canceled")
+//      }
+
+      onKeyPressed = (e:KeyEvent) => {
+        if(e.code.toString() == "UP") up = true
+        if(e.code.toString() == "DOWN") down = true
+        if(e.code.toString() == "LEFT") left = true
+        if(e.code.toString() == "RIGHT") right = true
       }
-      val result = playerlogin.showAndWait()
-      result match{
-        case Some(name) => println("Your name: " + name)
-        case None       => println("Dialog was canceled")
+      onKeyReleased = (e:KeyEvent) => {
+        if(e.code.toString() == "UP") up = false
+        if(e.code.toString() == "DOWN") down = false
+        if(e.code.toString() == "LEFT") left = false
+        if(e.code.toString() == "RIGHT") right = false
       }
-      content = picture
+
+      if(up) player.moveUp()
+      if(down) player.moveDown()
+      if(left) player.moveLeft()
+      if(right) player.moveRight()
+
+      player.display(g)
+
+
+      content = canvas
     }
   }
 }
