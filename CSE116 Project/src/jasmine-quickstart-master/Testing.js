2d38034369d888ada3f2a66b0252386bf118f332
Johnson_college <kalokho@buffalo.edu>
2019-05-10 00:01:26 -0400

    Test

---
+++ CSE116 Project/src/jasmine-quickstart-master/Testing.js
@@ -0,0 +1,23 @@
+describe('something', () =>{
+    it('does something', () =>{
+        expect(InitPosX(10)).toBe(10)
+    })
+});
+
+describe('something', () =>{
+    it('does something', () =>{
+        expect(InitPosY(120)).toBe(120)
+    })
+});
+
+describe('something', () =>{
+    it('does something', () =>{
+        expect(width(30)).toBe(30)
+    })
+});
+
+describe('something', () =>{
+    it('does something', () =>{
+        expect(height(30)).toBe(30)
+    })
+});
